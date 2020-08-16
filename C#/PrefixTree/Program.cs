ae5da47184259a5bffc9465441e669aa1ca90cff
Preshen Naidoo <preshenn@gmail.com>
2020-08-16 14:41:27 +0200

    Trie data structure
    Simple prefix tree data structure

---
+++ C#/PrefixTree/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrefixTree
+{
+    class Tries
+    {
+        TrieNode root;
+
+        public void CreateRoot()
+        {
+            root = new TrieNode();
+        }
+
+        public void Add(char[] chars)
+        {
+            TrieNode tempRoot = root;
+            int total = chars.Count() - 1;
+            for (int i = 0; i < chars.Count(); i++)
+            {
+                TrieNode newTrie;
+                if (tempRoot.children.Keys.Contains(chars[i]))
+                {
+                    tempRoot = tempRoot.children[chars[i]];
+                }
+                else
+                {
+                    newTrie = new TrieNode();
+
+                    if (total == i)
+                    {
+                        newTrie.endOfWord = true;
+                    }
+
+                    tempRoot.children.Add(chars[i], newTrie);
+                    tempRoot = newTrie;
+                }
+            }
+        }
+
+        public bool FindPrefix(char[] chars)
+        {
+            TrieNode tempRoot = root;
+            for (int i = 0; i < chars.Count(); i++)
+            {
+                if (tempRoot.children.Keys.Contains(chars[i]))
+                {
+                    tempRoot = tempRoot.children[chars[i]];
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool FindWord(char[] chars)
+        {
+            TrieNode tempRoot = root;
+            int total = chars.Count() - 1;
+            for (int i = 0; i < chars.Count(); i++)
+            {
+                if (tempRoot.children.Keys.Contains(chars[i]))
+                {
+                    tempRoot = tempRoot.children[chars[i]];
+
+                    if (total == i)
+                    {
+                        if (tempRoot.endOfWord == true)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+        public bool endOfWord;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tries t = new Tries();
+            t.CreateRoot();
+            t.Add("abc".ToCharArray());
+            t.Add("abcde".ToCharArray());            
+
+            bool findPrefix1 = t.FindPrefix("ab".ToCharArray());            
+
+            bool findWord1 = t.FindWord("abcde".ToCharArray());
+           
+        }
+    }
+}
